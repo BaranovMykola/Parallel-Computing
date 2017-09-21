7d319a10c940257404bfc3d4c94efe053d0f3664
BaranovMykola <mapsg32@gmail.com>
2017-09-21 22:16:23 +0300

    Implemented partitioning to 4th thread (preprocessing)

--- ParallelSorting/Sort.cpp
+++ ParallelSorting/Sort.cpp
@@ -3,6 +3,12 @@
 #include <vector>
 #include <thread>
 #include <algorithm>
+#include <queue>
+#include <future>
+#include <chrono>
+
+typedef std::pair<int, int> interval;
+using namespace std::chrono_literals;
 
 void quickSort(int arr[], int left, int right)
 {
@@ -34,26 +40,55 @@ void quickSort(int arr[], int left, int right)
 		quickSort(arr, i, right);
 }
 
+std::vector<interval> partialquickSort(int arr[], int left, int right)
+{
+	int i = left, j = right;
+	int tmp;
+	int pivot = arr[(left + right) / 2];
+
+	/* partition */
+	while (i <= j)
+	{
+		while (arr[i] < pivot)
+			i++;
+		while (arr[j] > pivot)
+			j--;
+		if (i <= j)
+		{
+			tmp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = tmp;
+			i++;
+			j--;
+		}
+	};
+
+	std::vector<interval> q;
+	/* recursion */
+	if (left < j)
+		//quickSort(arr, left, j);
+		q.push_back(std::make_pair(left, j));
+	if (i < right)
+		q.push_back(std::make_pair(i, right));
+	return q;
+}
+std::vector<interval> foo() { return std::vector<interval>(); }
+
 void quickSortParallel(int*& arr, int size)
 {
+	std::queue<interval> q;
 	int p = 4;
+	std::vector<std::thread> t;
+	std::vector<std::future<std::vector<interval>>> f;
+	
+	std::nth_element(arr, arr + size / 2, arr + size);
+	std::nth_element(arr, arr + size / 4, arr + size/2);
+	int thoffset = size*(2 / 3.0);
+	std::nth_element(arr+size/2, arr + (int)thoffset, arr + size);
 
-	std::vector<std::thread> thrs;
-	for (int i = 0; i < p; i++)
-	{
-		int from = (size / p)*i;
-		int to = (size / p)*(i+1);
-		if (i + 1 == p)
-		{
-			to = size - 1;
-		}
-		thrs.emplace_back(quickSort, arr, from, to);
-	}
-	for (auto& i : thrs)
-	{
-		i.join();
-	}
-	arr = merge(arr, size, p);
+
+	
+
 }
 
 int* merge(int* arr, int size, int p)
